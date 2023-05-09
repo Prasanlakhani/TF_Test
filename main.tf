fe777b381bb7b8aec1edb3deb3392562709d6d84
Prasan Lakhani <prasanlakhani@gmail.com>
2023-05-09 15:45:52 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.5"
+
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_name
+  #region  = var.region_name
+}
+
+
+
+terraform {
+  backend "gcs" {
+    bucket  = "niravgcp4-tf-state-prod"
+    prefix  = "terraform/state"
+  }
+}
+
+resource "google_compute_network" "mynetwork" {
+name = "mynetwork"
+project = "niravgcp4"
+auto_create_subnetworks = "true"
+}
+
+resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
+name = "mynetwork-allow-http-ssh-rdp-icmp"
+network = google_compute_network.mynetwork.self_link
+allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3389"]
+    }
+allow {
+    protocol = "icmp"
+    }
+source_ranges = ["0.0.0.0/0"]
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "testvm1"
+  zone         = "europe-west1-d"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      }
+  }
+
+
+}
+
+
+resource "google_storage_bucket" "bucket1" {
+    name                        = "prasan_a517965_2"
+    location                    = "US-CENTRAL1"
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = true
+}
+
+
+resource "google_storage_bucket" "bucket1" {
+    name                        = "prasan_a517965_2"
+    location                    = "US-CENTRAL1"
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = true
+}
