1bdc84b3ea8e52512d95d01ececfdf60462096ad
prasan <35463310+Prasanlakhani@users.noreply.github.com>
2023-05-30 14:47:29 +0000

    v7

--- Test.tfvars
+++ Test.tfvars
@@ -1,18 +1,18 @@
-instance_name      = "prasan-dev"
+#instance_name      = "prasan-dev"
 
 
 project = "nirav2gcp4"
 
 vpc_name           = "dev-vpc"
 
-vmzone = "us-central1-a"
+#vmzone = "us-central1-a"
 
 
 
 
-region = "europe-west2"
+#region = "europe-west2"
 
-backend_cloud_bucket = "nirav2gcp4-tf-state-test"
+#backend_cloud_bucket = "nirav2gcp4-tf-state-test"
 
 
-tf_workspace = "TF_Test"
+#tf_workspace = "TF_Test"
--- variables.tf
+++ variables.tf
@@ -7,20 +7,20 @@ variable "vpc_name" {
   type = string
 }
 
-variable "backend_bucket" {
-  type = string
-}
+#variable "backend_bucket" {
+#  type = string
+#}
 
 
-variable "backend_cloud_bucket" {
-  type = string
-}
+#variable "backend_cloud_bucket" {
+#  type = string
+#}
 
-variable "tf_workspace" {
-  type = string
-}
+#variable "tf_workspace" {
+#  type = string
+#}
 
-variable "instance_name" {
-  type = string
-}
+#variable "instance_name" {
+#  type = string
+#}
 
